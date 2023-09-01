4642285fbd0cb04a5ac3eee6a2677db31efe6284
lysnost <135021509+MyFriendHowAreYou@users.noreply.github.com>
2023-09-01 19:53:29 +0100

    Update SlotFlags.cs

--- Content.Shared/Inventory/SlotFlags.cs
+++ Content.Shared/Inventory/SlotFlags.cs
@@ -26,5 +26,8 @@ public enum SlotFlags
     LEGS = 1 << 13,
     FEET = 1 << 14,
     SUITSTORAGE = 1 << 15,
+	UNDERWEART = 1 << 10, // Трусики
+    UNDERWEARB = 1 << 10, // Трусики
+    SOCKS = 1 << 10,      // Трусики
     All = ~NONE,
 }
